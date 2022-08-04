4f81e1b5f1e3c9a5393e96b8afa2d072018d23e7
Ju Hyeon <aganga7427@gmail.com>
2022-08-04 22:53:53 +0900

    feat: add cors policy

--- back/blog/blog/settings.py
+++ back/blog/blog/settings.py
@@ -9,6 +9,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
+
 INSTALLED_APPS = [
     'post.apps.PostConfig',
     'note.apps.NoteConfig',
@@ -22,6 +24,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
