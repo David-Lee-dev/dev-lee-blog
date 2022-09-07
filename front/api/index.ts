b8e8fb433ea190d446929b693f80d4ebc3c74316
Ju Hyeon <aganga7427@gmail.com>
2022-09-07 10:26:06 +0900

    fix: base url 수정

--- front/api/index.ts
+++ front/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://www.im-dev-lee.site/';
-const BASE_URL = 'http://localhost:8000/';
+const BASE_URL = 'http://www.im-dev-lee.site/';
+// const BASE_URL = 'http://localhost:8000/';
 
 const createAxiosInstance = () => {
   const axiosInstance = axios.create({
