eb05e2a507b03c6df14b48c2dac83d528aa19a4e
Ju Hyeon <aganga7427@gmail.com>
2022-11-29 14:29:15 +0900

    fix: build error

--- notion-blog/library/notion/getBlockPath.ts
+++ notion-blog/library/notion/getBlockPath.ts
@@ -1,17 +1,23 @@
-import { Block, Page } from '../../types/notion_api_types';
+import { notion } from '.';
+import { Block, Page, ParentPage } from '../../types/notion_api_types';
 import { getBlock } from './getBlock';
 import { getPage } from './getPage';
 
 export const getBlockPath = async (notionOjbect: Page | Block): Promise<Page[]> => {
   if (notionOjbect.id === process.env.NEXT_NOTION_DATABASE_ID) return [];
 
-  if (notionOjbect.parent.type === 'block_id') {
-    const parentBlock = (await getBlock(notionOjbect.parent.block_id)) as Block;
-    return [...(await getBlockPath(parentBlock))];
-  } else if (notionOjbect.parent.type === 'page_id') {
-    const parentPage = (await getPage(notionOjbect.parent.page_id)) as Page;
-    return [notionOjbect as Page, ...(await getBlockPath(parentPage))];
+  const result = [];
+  if (notionOjbect.object === 'page') result.push(notionOjbect);
+
+  let nextObject = Object();
+  switch (notionOjbect.parent.type) {
+    case 'block_id':
+      nextObject = (await getBlock(notionOjbect.parent.block_id)) as Block;
+      break;
+    case 'page_id':
+      nextObject = (await getPage(notionOjbect.parent.page_id)) as Page;
+      break;
   }
 
-  return [];
+  return [...(await getBlockPath(nextObject)), ...result];
 };
